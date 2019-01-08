4372e374eebaeb07432a010cb2625c48ca884ca8
Salieri <gitea@fake.local>
2019-01-08 20:33:42 +1300

    Done "Hello Window"

--- Glitter/main.cpp
+++ Glitter/main.cpp
@@ -9,6 +9,16 @@
 #include <cstdio>
 #include <cstdlib>
 
+
+void framebuffer_size_callback(GLFWwindow *win, int w, int h) {
+    glViewport(0, 0, w, h);
+}
+
+void processInput(GLFWwindow *win) {
+    if(glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(win, true);
+}
+
 int main(int argc, char * argv[]) {
 
     // Load GLFW and Create a Window
@@ -31,13 +41,16 @@ int main(int argc, char * argv[]) {
     gladLoadGL();
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
+    glViewport(0, 0, mWidth, mHeight);
+
+    glfwSetFramebufferSizeCallback(mWindow, framebuffer_size_callback);
+
     // Rendering Loop
-    while (glfwWindowShouldClose(mWindow) == false) {
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(mWindow, true);
+    while (!glfwWindowShouldClose(mWindow)) {
+        processInput(mWindow);
 
         // Background Fill Color
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+        glClearColor(1.0f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Flip Buffers and Draw
